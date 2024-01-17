92fac6dc33f2687e7402425dc633fe2aa4233eca
ellabalter <ella.balter@post.runi.ac.il>
2024-01-17 14:10:38 +0200

    try 3

--- GameOfLife.java
+++ GameOfLife.java
@@ -164,9 +164,9 @@ public class GameOfLife {
     public static void print(int[][] arr) {
 		for(int i = 0; i < arr.length; i++){
 			for(int j = 0; j < arr[0].length; j++){
-				System.out.printf("%d ", arr[i][j]);
+				System.out.printf("%d  ", arr[i][j]);
 			}
-			System.out.printf("%n");
+			System.out.printf("%n ");
 		}
 	}
 		
